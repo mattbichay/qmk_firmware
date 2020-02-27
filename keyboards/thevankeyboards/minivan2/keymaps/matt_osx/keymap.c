4870d4d5853ba2ed96a6fd4b05845cc8edd4936d
Matthew Bichay <matt@Matthews-iPro.fios-router.home>
2020-02-26 19:42:57 -0500

    Swapped layers one and two, made esc more intuitive.

--- keyboards/thevankeyboards/minivan2/keymaps/matt_osx/keymap.c
+++ keyboards/thevankeyboards/minivan2/keymaps/matt_osx/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, TD(BSPC_KEY_TAP),
                TD(LYR1_KEY_TAP), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
                KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-               KC_LCTL, KC_LALT, KC_LGUI, KC_NO, LT(LAYER_2, KC_SPC), KC_SPC, LT(LAYER_3, KC_PGUP), KC_PGDN, JMP_LEFT, JMP_RIGHT),
+               KC_LCTL, KC_LALT, KC_LGUI, KC_NO, LT(LAYER_3, KC_SPC), KC_SPC, LT(LAYER_2, KC_PGDN), KC_PGUP, JMP_LEFT, JMP_RIGHT),
  
   [1] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
                KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,
@@ -100,13 +100,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT(KC_ESC, KC_LPRN, KC_RPRN, KC_PLUS, KC_MINS, KC_NO, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_TRNS,
                KC_TRNS, KC_LBRC, KC_RBRC, KC_ASTR, KC_SLSH, KC_NO, KC_NO, KC_DOT, KC_4, KC_5, KC_6, KC_TRNS,
-               KC_TRNS, KC_LT, KC_GT, KC_CIRC, KC_EQL, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_UP, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_0, KC_LEFT, KC_DOWN, KC_RIGHT),
+               KC_TRNS, KC_LT, KC_GT, KC_CIRC, KC_EQL, KC_NO, KC_NO, KC_0, KC_1, KC_2, KC_3, KC_TRNS,
+               KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS),
 
-  [3] = LAYOUT(KC_NO, KC_SLCK, KC_PAUS, KC_F16, KC_F17, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F18, OSX_LOCK,
-               KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TD(MCRO_KEY_TAP),
-               KC_MPLY, KC_MPRV, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-               RESET, KC_NO, KC_NO, KC_NO, TD(LANG_KEY_TAP), KC_NO, KC_TRNS, KC_NO, KC_HOME, KC_END) 
+  [3] = LAYOUT(KC_NO, KC_F16, KC_F17, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F18, OSX_LOCK,
+               KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MPLY, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TD(MCRO_KEY_TAP),
+               KC_NO, KC_SLCK, KC_PAUS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_UP, KC_END,
+               RESET, KC_NO, KC_NO, KC_NO, KC_NO, TD(LANG_KEY_TAP), KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT) 
 
 };
 
@@ -158,8 +158,11 @@ void lyr1_key_finished(qk_tap_dance_state_t *state, void *user_data)
     switch (lyr1_tap_state.state)
     {
 		case DOUBLE_TAP:
-			tap_code(KC_CAPS);
+			tap_code(KC_ESC);
 			break;
+        case TRIPLE_TAP:
+            tap_code(KC_CAPS);
+            break;
     }
 }
 
@@ -262,15 +265,15 @@ void process_indicator_update(uint32_t state, uint8_t usb_led) {
   setrgb(0, 0, 255, (LED_TYPE *)&led[0]);
 }
 
-    if (state & (1<<3)) {
+    if (state & (1<<2)) {
   setrgb(255, 182, 193, (LED_TYPE *)&led[0]);
 }
 
-  if (state & (1<<2)) {
+  if (state & (1<<3)) {
   setrgb(0, 255, 255, (LED_TYPE *)&led[1]);
 }
 
-    if (state & (1<<3)) {
+    if (state & (1<<2)) {
   setrgb(255, 182, 193, (LED_TYPE *)&led[1]);
 }
 
