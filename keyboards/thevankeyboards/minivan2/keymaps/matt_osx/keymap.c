69ca6bee12416e71aefc6833d13af1414ffa94ab
Matthew Bichay <matt@Matthews-iPro.fios-router.home>
2020-03-15 00:01:09 -0400

    Cleaned up a little bit, made closer to V4N setup

--- keyboards/thevankeyboards/minivan2/keymaps/matt_osx/keymap.c
+++ keyboards/thevankeyboards/minivan2/keymaps/matt_osx/keymap.c
@@ -5,14 +5,48 @@
 #define JMP_LEFT LALT(KC_LEFT)
 #define JMP_RIGHT LALT(KC_RIGHT)
 
-// TAP STATE Definition
-typedef struct
-{
+// Layer Names
+enum {
+    BASE_LAYER = 0,
+    NUMROW_SYMBOL_LAYER = 1,
+    NAV_LAYER = 2,
+    NUMPAD_FN_LAYER = 3
+};
+
+// Macro Names
+enum {
+    BSPC_SHFT_DEL = SAFE_RANGE,
+    PGDN_SHFT_PGUP,
+};
+
+// Tapdance Names
+enum {
+    UPPER_FN_TAP = 0,
+    LOWER_FN_TAP = 1,
+    DY_MCRO1_TAP = 2,
+    RIGHT_UP_TAP = 3,
+    LEFT_DOWN_TAP = 4
+};
+
+// Tap Dance Function Prototypes
+void upper_fn_key_press(qk_tap_dance_state_t *state, void *user_data);
+void upper_fn_key_finished(qk_tap_dance_state_t *state, void *user_data);
+void upper_fn_key_reset (qk_tap_dance_state_t *state, void *user_data);
+
+void lower_fn_key_press(qk_tap_dance_state_t *state, void *user_data);
+void lower_fn_key_finished(qk_tap_dance_state_t *state, void *user_data);
+void lower_fn_key_reset (qk_tap_dance_state_t *state, void *user_data);
+
+void mcro_key1_finished(qk_tap_dance_state_t *state, void *user_data);
+void mcro_key_finished(qk_tap_dance_state_t *state, void *user_data);
+void mcro_key_reset(qk_tap_dance_state_t *state, void *user_data);
+
+// Tap Dance States
+typedef struct {
     bool is_press_action;
     int state;
 } tap;
 
-// TAP DANCE STATES
 enum {
   SINGLE_TAP = 1,
   SINGLE_HOLD = 2,
@@ -23,141 +57,88 @@ enum {
   TRIPLE_HOLD = 7
 };
 
-
-// TAP DANCE ENUMS
-enum
-{
-    LYR1_KEY_TAP = 0,
-    BSPC_KEY_TAP = 1,
-    MCRO_KEY_TAP = 2,
-    LANG_KEY_TAP = 3
-};
-
-enum
-{
-    LAYER_0 = 0,
-    LAYER_1 = 1,
-    LAYER_2 = 2,
-    LAYER_3 = 3
-};
-
-
-//Helper PROTOTYPES
-int cur_dance (qk_tap_dance_state_t *state);
-
-//LYR1 Key Function Prototypes
-void lyr1_key_press(qk_tap_dance_state_t *state, void *user_data);
-void lyr1_key_finished(qk_tap_dance_state_t *state, void *user_data);
-void lyr1_key_reset (qk_tap_dance_state_t *state, void *user_data);
-
-
-//BSPC_DEL Key Function Prototypes
-void bspc_key_press(qk_tap_dance_state_t *state, void *user_data);
-void bspc_key_reset(qk_tap_dance_state_t *state, void *user_data);
-
-//MCRO Key Function Prototypes
-void mcro_key_finished(qk_tap_dance_state_t *state, void *user_data);
-void mcro_key_reset(qk_tap_dance_state_t *state, void *user_data);
-
-// Key Function Prototypes
-void lang_key_toggle(qk_tap_dance_state_t *state, void *user_data);
-
-
-enum custom_keycodes {
-  M_IME = SAFE_RANGE
-};
-
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    switch(keycode) {
-      case M_IME:
-        SEND_STRING(SS_DOWN(X_LSHIFT)SS_DOWN(X_LALT));
-        return false;
-    }
-  }
-  else {
-    switch(keycode) {
-      case M_IME:
-        SEND_STRING(SS_UP(X_LSHIFT)SS_UP(X_LALT));
-        return false;
-    }
-  }
-  return true;
-};
+// Helper Function Prototypes
+int cur_dance(qk_tap_dance_state_t *state);
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    
+    [BASE_LAYER] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, BSPC_SHFT_DEL,
+            TD(UPPER_FN_TAP), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+            KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+            KC_LCTL, KC_LALT, KC_LGUI, KC_NO, LT(NAV_LAYER, KC_SPC), KC_SPC, TD(LOWER_FN_TAP), TD(LEFT_DOWN_TAP), KC_NO, TD(RIGHT_UP_TAP)),
 
-  [0] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, TD(BSPC_KEY_TAP),
-               TD(LYR1_KEY_TAP), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-               KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_UP),
-               KC_LCTL, KC_LALT, KC_LGUI, KC_NO, LT(LAYER_3, KC_SPC), KC_SPC, LT(LAYER_2, KC_PGDN), KC_PGUP, JMP_LEFT, JMP_RIGHT),
- 
-  [1] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
-               KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_QUOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_EQL, KC_BSLS, KC_TRNS, KC_TRNS),
+    [NUMROW_SYMBOL_LAYER] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_QUOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_EQL, KC_BSLS, KC_NO, KC_TRNS),
 
-  [2] = LAYOUT(KC_ESC, KC_LPRN, KC_RPRN, KC_PLUS, KC_MINS, KC_NO, RESET, KC_NO, KC_7, KC_8, KC_9, KC_TRNS,
-               KC_TRNS, KC_LBRC, KC_RBRC, KC_ASTR, KC_SLSH, KC_NO, KC_NO, KC_DOT, KC_4, KC_5, KC_6, KC_TRNS,
-               KC_TRNS, KC_LT, KC_GT, KC_CIRC, KC_EQL, KC_NO, KC_NO, KC_0, KC_1, KC_2, KC_3, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS),
-
-  [3] = LAYOUT(KC_NO, KC_F16, KC_F17, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, OSX_PSCR, OSX_LOCK,
-               KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MPLY, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TD(MCRO_KEY_TAP),
-               KC_NO, KC_SLCK, KC_PAUS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_UP, KC_END,
-               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TD(LANG_KEY_TAP), KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT) 
-
+    [NAV_LAYER] = LAYOUT(KC_TRNS, KC_F16, KC_F17, OSX_PSCR, OSX_LOCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_SLCK, KC_PAUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_UP, KC_END, PGDN_SHFT_PGUP,
+            KC_MPRV, KC_MNXT, KC_MPLY, KC_NO, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_NO, KC_RIGHT),
+    
+    [NUMPAD_FN_LAYER] = LAYOUT(RESET, KC_PPLS, KC_PMNS, KC_LPRN, KC_RPRN, KC_PERC, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_TRNS,
+            KC_PEQL, KC_PAST, KC_PSLS, KC_LBRC, KC_RBRC, KC_HASH, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_TRNS, 
+            KC_CIRC, KC_LT, KC_GT, KC_LCBR, KC_RCBR, KC_DLR, KC_NO, KC_P0, KC_P1, KC_P2, KC_P3, KC_PDOT, 
+            KC_EXLM, KC_AMPR, KC_PIPE, KC_NO, TD(DY_MCRO1_TAP), KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_COMM)
 };
 
 
-//Determine the current tap dance state
-int cur_dance (qk_tap_dance_state_t *state) {
-  if (state->count == 1) {
-    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
-    //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
-    else return SINGLE_HOLD;
-  }
-  else if (state->count == 2) {
-    /*
-     * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
-     * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
-     * keystrokes of the key, and not the 'double tap' action/macro.
-    */
-    if (state->interrupted) return DOUBLE_SINGLE_TAP;
-    else if (state->pressed) return DOUBLE_HOLD;
-    else return DOUBLE_TAP;
-  }
-  //Assumes no one is trying to type the same letter three times (at least not quickly).
-  //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
-  //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
-  if (state->count == 3) {
-    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
-    else return TRIPLE_HOLD;
-  }
-  else return 8; //magic number. At some point this method will expand to work for more presses
-}
+// Macro State
+static uint8_t BSPC_SHFT_DEL_KEYCODE = KC_BSPC;
+static uint8_t PGDN_SHFT_PGUP_KEYCODE = KC_PGDN;
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
-/* BEGIN LYR1_KEY_TAP */
-static tap lyr1_tap_state =
-{
+    switch (keycode) {
+        case BSPC_SHFT_DEL:
+            if (record->event.pressed) {
+                // If Left-Shift or Right-Shift is held
+                if (keyboard_report->mods & MOD_BIT(KC_RSFT) || keyboard_report->mods & MOD_BIT(KC_LSFT)) {
+                    // Set key to delete
+                    BSPC_SHFT_DEL_KEYCODE = KC_DEL;
+                } else {
+                    // Otherwise set key to backspace
+                    BSPC_SHFT_DEL_KEYCODE = KC_BSPC;
+                }
+                register_code(BSPC_SHFT_DEL_KEYCODE);
+            } else {
+                unregister_code(BSPC_SHFT_DEL_KEYCODE);
+            }
+            break;
+
+        case PGDN_SHFT_PGUP:
+            if (record->event.pressed) {
+                // If Left-Shift or Right-Shift is held
+                if (keyboard_report->mods & MOD_BIT(KC_RSFT) || keyboard_report->mods & MOD_BIT(KC_LSFT)) {
+                    // Set key to page up
+                    PGDN_SHFT_PGUP_KEYCODE = KC_PGUP;
+                } else {
+                    // Otherwise set key to page down
+                    PGDN_SHFT_PGUP_KEYCODE = KC_PGDN;
+                }
+                register_code(PGDN_SHFT_PGUP_KEYCODE);
+            } else {
+                unregister_code(PGDN_SHFT_PGUP_KEYCODE);
+            }
+            break;
+    }
+    return true;
+};
+
+static tap upper_fn_tap_state = {
     .is_press_action = true,
     .state = 0
 };
 
-
-void lyr1_key_press(qk_tap_dance_state_t *state, void *user_data)
-{
-    layer_on(LAYER_1);
+void upper_fn_key_press(qk_tap_dance_state_t *state, void *user_data) {
+    layer_on(NUMROW_SYMBOL_LAYER);
 }
 
-void lyr1_key_finished(qk_tap_dance_state_t *state, void *user_data)
-{
-    lyr1_tap_state.state = cur_dance(state);
-    switch (lyr1_tap_state.state)
-    {
+void upper_fn_key_finished(qk_tap_dance_state_t *state, void *user_data) {
+    upper_fn_tap_state.state = cur_dance(state);
+    switch (upper_fn_tap_state.state) {
 		case DOUBLE_TAP:
 			tap_code(KC_ESC);
 			break;
@@ -167,37 +148,42 @@ void lyr1_key_finished(qk_tap_dance_state_t *state, void *user_data)
     }
 }
 
-void lyr1_key_reset(qk_tap_dance_state_t *state, void *user_data)
-{
-    layer_off(LAYER_1);
-    lyr1_tap_state.state = 0;
-}
-/* END LYR1_KEY_TAP */
-
-
-
-/* BEGIN BPSC_KEY_TAP */
-
-static uint8_t del_bspc_lastcode = KC_NO;
-
-void bspc_key_reset(qk_tap_dance_state_t *state, void *user_data)
-{
-    unregister_code(del_bspc_lastcode);
+void upper_fn_key_reset (qk_tap_dance_state_t *state, void *user_data) {
+    layer_off(NUMROW_SYMBOL_LAYER);
+    upper_fn_tap_state.state = 0;
 }
 
-void bspc_key_press(qk_tap_dance_state_t *state, void *user_data)
-{
-    if (keyboard_report->mods & MOD_BIT (KC_RSFT) || keyboard_report->mods & MOD_BIT(KC_LSFT))
-        del_bspc_lastcode = KC_DEL;
-    else
-        del_bspc_lastcode = KC_BSPC;
 
-    register_code(del_bspc_lastcode);
+static tap lower_fn_tap_state = {
+    .is_press_action = true,
+    .state = 0
+};
+
+void lower_fn_key_press(qk_tap_dance_state_t *state, void *user_data) {
 }
 
-/* END BSPC_KEY_TAP */
+void lower_fn_key_finished(qk_tap_dance_state_t *state, void *user_data) {
+    lower_fn_tap_state.state = cur_dance(state);
+    switch (lower_fn_tap_state.state) {
+        case SINGLE_HOLD:
+            layer_on(NUMPAD_FN_LAYER);
+            break;
 
-/* BEGIN MCRO KEY TAP */
+        case DOUBLE_TAP:
+            if (layer_state_is(NUMPAD_FN_LAYER)) {
+                layer_off(NUMPAD_FN_LAYER);
+            } else {
+                layer_on(NUMPAD_FN_LAYER);
+            }
+            break;
+    }
+}
+
+void lower_fn_key_reset (qk_tap_dance_state_t *state, void *user_data) {
+    if (lower_fn_tap_state.state == SINGLE_HOLD)
+        layer_off(NUMPAD_FN_LAYER);
+    lower_fn_tap_state.state = 0;
+}
 
 
 static tap mcro_tap_state =
@@ -207,104 +193,109 @@ static tap mcro_tap_state =
 };
 
 static bool recording = false;
-void mcro_key_finished(qk_tap_dance_state_t *state, void *user_data)
-{
+static int DYN_MACRO_PLAY = DYN_MACRO_PLAY1;
+static int DYN_REC_START = DYN_REC_START1;
+
+void mcro_key1_finished(qk_tap_dance_state_t *state, void *user_data) {
+    DYN_MACRO_PLAY = DYN_MACRO_PLAY1;
+    DYN_REC_START = DYN_REC_START1;
+    mcro_key_finished(state, user_data);
+}
+
+void mcro_key_finished(qk_tap_dance_state_t *state, void *user_data) {
     mcro_tap_state.state = cur_dance(state);
     keyrecord_t kr;
     kr.event.pressed = false;
 
-    if (recording)
-    {
+    if (recording) {
         kr.event.pressed = true;
         process_dynamic_macro(DYN_REC_STOP, &kr);
         recording = false;
     }
-    else if (mcro_tap_state.state == SINGLE_TAP)
-    {
+
+    else if (mcro_tap_state.state == SINGLE_TAP) {
         kr.event.pressed = false;
-        process_dynamic_macro(DYN_MACRO_PLAY1, &kr);
+        process_dynamic_macro(DYN_MACRO_PLAY, &kr);
     }
-    else if (mcro_tap_state.state == DOUBLE_TAP)
-    {
+
+    else if (mcro_tap_state.state == DOUBLE_TAP) {
         kr.event.pressed = false;
         recording = true;
-        process_dynamic_macro(DYN_REC_START1, &kr);
+        process_dynamic_macro(DYN_REC_START, &kr);
     }
 }
 
-void mcro_key_reset (qk_tap_dance_state_t *state, void *user_data)
-{
+void mcro_key_reset(qk_tap_dance_state_t *state, void *user_data) {
     mcro_tap_state.state = 0;
 }
-/* END MCRO KEY TAP */
-
-/* BEGIN LANG KEY TAP */
-
-static bool japanese_on = false; 
-
-void lang_key_toggle(qk_tap_dance_state_t *state, void *user_data)
-{
-    if (japanese_on)
-    {
-        tap_code(KC_LANG2);
-        japanese_on = false;
-    }
-    else
-    {
-        tap_code(KC_LANG1);
-        japanese_on = true;
-    }
-}
-/* END LANG KEY TAP */
-
-
-void process_indicator_update(uint32_t state, uint8_t usb_led) {
-  for (int i = 0; i < 3; i++) {
-    setrgb(0, 0, 0, (LED_TYPE *)&led[i]);
-  }
-  if (state & (1<<1)) {
-  setrgb(0, 0, 255, (LED_TYPE *)&led[0]);
-}
-
-    if (state & (1<<2)) {
-  setrgb(255, 182, 193, (LED_TYPE *)&led[0]);
-}
-
-  if (state & (1<<3)) {
-  setrgb(0, 255, 255, (LED_TYPE *)&led[1]);
-}
-
-    if (state & (1<<2)) {
-  setrgb(255, 182, 193, (LED_TYPE *)&led[1]);
-}
-
-  if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-  setrgb(255, 0, 0, (LED_TYPE *)&led[2]);
-}
-
-
-  rgblight_set();
-};
-
-void keyboard_post_init_user(void) {
-  process_indicator_update(layer_state, host_keyboard_leds());
-};
-
-void led_set_user(uint8_t usb_led) {
-  process_indicator_update(layer_state, host_keyboard_leds());
-};
-
-uint32_t layer_state_set_user(uint32_t state) {
-  process_indicator_update(state, host_keyboard_leds());
-  return state;
-};
 
 
 //Associate our tap dance key with its functionality
-qk_tap_dance_action_t tap_dance_actions[] = 
-{
-    [LYR1_KEY_TAP] = ACTION_TAP_DANCE_FN_ADVANCED(lyr1_key_press, lyr1_key_finished, lyr1_key_reset),
-    [BSPC_KEY_TAP] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(bspc_key_press, NULL, bspc_key_reset, 50),
-    [MCRO_KEY_TAP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, mcro_key_finished, mcro_key_reset),
-    [LANG_KEY_TAP] = ACTION_TAP_DANCE_FN(lang_key_toggle)
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [UPPER_FN_TAP] = ACTION_TAP_DANCE_FN_ADVANCED(upper_fn_key_press, upper_fn_key_finished, upper_fn_key_reset),
+    [LOWER_FN_TAP] = ACTION_TAP_DANCE_FN_ADVANCED(lower_fn_key_press, lower_fn_key_finished, lower_fn_key_reset),
+    [DY_MCRO1_TAP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, mcro_key_finished, mcro_key_reset),
+    [RIGHT_UP_TAP] = ACTION_TAP_DANCE_DOUBLE(KC_RIGHT, KC_UP),
+    [LEFT_DOWN_TAP] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_DOWN)
 };
+
+
+void process_indicator_update(uint32_t state, uint8_t usb_led) {
+    for (int i = 0; i < 3; i++) {
+        setrgb(0, 0, 0, (LED_TYPE *)&led[i]);
+    }
+    if (state & (1<<NUMROW_SYMBOL_LAYER)) {
+        setrgb(0, 0, 255, (LED_TYPE *)&led[0]);
+    }
+    if (state & (1<<NAV_LAYER)) {
+        setrgb(128, 0, 128, (LED_TYPE *)&led[0]);
+    }
+    if (state & (1<<NUMPAD_FN_LAYER)) {
+        setrgb(0, 255, 255, (LED_TYPE *)&led[1]);
+    }
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+        setrgb(255, 0, 0, (LED_TYPE *)&led[2]);
+    }
+    rgblight_set();
+};
+
+void keyboard_post_init_user(void) {
+    process_indicator_update(layer_state, host_keyboard_leds());
+};
+
+void led_set_user(uint8_t usb_led) {
+    process_indicator_update(layer_state, host_keyboard_leds());
+};
+
+uint32_t layer_state_set_user(uint32_t state) {
+    process_indicator_update(state, host_keyboard_leds());
+    return state;
+};
+
+//Determine the current tap dance state
+int cur_dance (qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed)  return SINGLE_TAP;
+        //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
+        else return SINGLE_HOLD;
+    }
+    else if (state->count == 2) {
+        /*
+        * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        * keystrokes of the key, and not the 'double tap' action/macro.
+        */
+    if (state->interrupted) return DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return DOUBLE_HOLD;
+        else return DOUBLE_TAP;
+    }
+    //Assumes no one is trying to type the same letter three times (at least not quickly).
+    //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+    //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
+        else return TRIPLE_HOLD;
+    }
+    else return 8; //magic number. At some point this method will expand to work for more presses
+}
+
